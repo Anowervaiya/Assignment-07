df44fec9c22edf806e5fc371c735b2629849bb11
Anowervaiya <mdanowerhossen727@gmail.com>
2024-03-16 21:56:30 +0600

    angry mood

--- A7/src/App.jsx
+++ A7/src/App.jsx
@@ -1,41 +1,13 @@
-
-
-import './App.css'
+import './App.css';
+import Navbar from './component/navbar/Navbar';
 
 function App() {
-  
-
   return (
-    <>
-      <h1>Assingment-07</h1>
-
-      <div class="navbar bg-base-100">
-        <div class="flex-1">
-          <a class="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div class="flex-none">
-          <ul class="menu menu-horizontal px-1">
-            <li>
-              <a>Link</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul class="p-2 bg-base-100 rounded-t-none">
-                  <li>
-                    <a>Link 1</a>
-                  </li>
-                  <li>
-                    <a>Link 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </>
+    <div>
+      <h1>anower hoossen</h1>
+      <Navbar></Navbar>
+    </div>
   );
 }
 
-export default App
+export default App;
---
+++ A7/src/component/navbar/Navbar.jsx
@@ -0,0 +1,6 @@
+const Navbar = () => {
+  return (
+    <h1>Anower vai</h1>
+  )
+};
+export default Navbar;
